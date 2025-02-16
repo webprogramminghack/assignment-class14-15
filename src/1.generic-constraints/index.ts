bb3bd5ad59496a282fe973f47c8b0a8add2ba646
Dion <prep.prodigy1@gmail.com>
2025-02-16 21:02:00 +0700

    Revisi assigment14-15

--- src/1.generic-constraints/index.ts
+++ src/1.generic-constraints/index.ts
@@ -4,11 +4,16 @@
 // You can remove the readonly modifier from the mapped type by using -readonly
 
 // ------- start coding here -------
+// Menggunakan type sendiri dan menggunakan -readonly
+type Mapping<S, M extends { [K in keyof M]: keyof S }> = {
+  -readonly [K in keyof M]: S[M[K]];
+};
+
 function mapObjectProperties<
   S extends object,
   M extends { [K in keyof M]: keyof S }
->(source: S, mapping: M): { [K in keyof M]: S[M[K]] } {
-  const result = {} as { [K in keyof M]: S[M[K]] };
+>(source: S, mapping: M): Mapping<S, M> {
+  const result = {} as Mapping<S, M>;
 
   for (const key in mapping) {
     result[key] = source[mapping[key]];
@@ -58,3 +63,7 @@ const transformedUser2 = mapObjectProperties(user, mapping2);
 // }
 
 const transformedUser3 = mapObjectProperties(user, mapping3); // should produce error since 'doesntExist' is not a key of user
+
+console.log(transformedUser1);
+console.log(transformedUser2);
+console.log(transformedUser3);
